c9cd6e60765a3ecc4dd4bb2a16fb2e6b5df82077
julien31210 <julien.linder31@gmail.com>
2021-03-06 15:41:05 +0100

    resolve 2nd workshop and add camera controller script

--- Assets/01_SCRIPTS/01_CameraScript/CameraController.cs
+++ Assets/01_SCRIPTS/01_CameraScript/CameraController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Animator animator;
+    public GameObject Camera;
     public GameObject Player;
     // Start is called before the first frame update
     void Start()
@@ -19,16 +19,35 @@ public class CameraController : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
+
+
         if (other.CompareTag("CameraChange"))
         {
             Debug.Log("touché");
-            animator.Play("Camera 1");
+            cameraMove();
         }
 
         if (other.CompareTag("CameraReset"))
         {
             Debug.Log("touché");
-            animator.Play("Camera 2");
+            cameraReset();
         }
+    }
+    private void cameraMove()
+    {
+        LeanTween.moveZ(Camera, 12, 8).setEaseOutCirc();
+        LeanTween.moveY(Camera, 4, 8).setEaseOutCirc();
+        LeanTween.moveX(Camera, 1, 10).setEaseOutCirc();
+        LeanTween.rotateZ(Camera, 90, 8).setEaseOutCirc();
+        LeanTween.rotateY(Camera, 90, 8).setEaseOutCirc();
+        LeanTween.rotateX(Camera, 90, 8).setEaseOutCirc();
+    }
+
+    private void cameraReset()
+    {
+        LeanTween.moveZ(Camera, 0, 0).setEaseOutCirc();
+        LeanTween.moveZ(Camera, 0, 0).setEaseOutCirc();
+        LeanTween.moveX(Camera, 0, 0).setEaseOutCirc();
+        LeanTween.rotateY(Camera, 0, 0).setEaseOutCirc();
     }
 }
---
+++ Assets/01_SCRIPTS/02_OpposantScript/MovePlateform.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlateform : MonoBehaviour
+{
+    public GameObject child;
+    public Transform parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void MovePlateformasChild(Transform newParent)
+    {
+        child.transform.SetParent(newParent);
+
+        child.transform.SetParent(newParent, false);
+
+        child.transform.SetParent(null);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            MovePlateformasChild(collision.transform);
+        }
+    }
+}
