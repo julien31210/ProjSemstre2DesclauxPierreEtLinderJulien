327e70ca1ffd854c213a5d03d0257706759ba4b3
julien31210 <julien.linder31@gmail.com>
2021-03-10 09:32:48 +0100

    workshop 3 add

--- Assets/01_SCRIPTS/01_CameraScript/CameraController.cs
+++ Assets/01_SCRIPTS/01_CameraScript/CameraController.cs
@@ -6,10 +6,15 @@ public class CameraController : MonoBehaviour
 {
     public GameObject Camera;
     public GameObject Player;
+
+    Vector3 defaultPos;
+    Vector3 defaultRot;
+
+    Transform Cam;
     // Start is called before the first frame update
     void Start()
     {
-
+        backupTransform();
     }
 
     // Update is called once per frame
@@ -29,22 +34,35 @@ public class CameraController : MonoBehaviour
 
         if (other.CompareTag("CameraReset"))
         {
-            //Debug.Log("touché");
+            Debug.Log("touché connard");
             cameraReset();
         }
     }
     private void cameraMove()
     {
         Debug.Log("Salut trou duc");
-        LeanTween.moveLocal(Camera, new Vector3(1, 4, -10), 2).setEaseOutCirc();
-        LeanTween.rotateY(Camera, 360, 2).setEaseOutCirc();
+        LeanTween.moveLocal(Camera, new Vector3(1, 6, -15), 6).setEaseOutCirc();
+        LeanTween.rotateY(Camera, 360, 8).setEaseOutCirc();
     }
 
     private void cameraReset()
     {
-        /* LeanTween.moveZ(Camera, 0, 0).setEaseOutCirc();
-         LeanTween.moveZ(Camera, 0, 0).setEaseOutCirc();
-         LeanTween.moveX(Camera, 0, 0).setEaseOutCirc();
-         LeanTween.rotateY(Camera, 0, 0).setEaseOutCirc();*/
+
+        Debug.Log("Salut trou duc");
+        //Restore the all the original pos, scale and rot  of each GameOBject
+        LeanTween.moveLocal(Camera, defaultPos, 8).setEaseOutCirc();
+        LeanTween.rotateY(Camera, 90, 8).setEaseOutCirc();
+        Cam.localPosition = defaultPos;
+        Cam.localEulerAngles = defaultRot;
+
+    }
+    private void backupTransform()
+    {
+        GameObject tempModels = GameObject.FindGameObjectWithTag("MainCamera");
+
+        defaultPos = tempModels.transform.localPosition;
+        defaultRot = tempModels.transform.localEulerAngles;
+
+        Cam = gameObject.transform.GetChild(0).transform;
     }
 }
