bed35e91c44b5b4663e483ca95bf3baca06dabc1
julien31210 <julien.linder31@gmail.com>
2021-03-10 15:54:41 +0100

    add player death, for workshop 4

---
+++ Assets/01_SCRIPTS/02_OpposantScript/PlayerDeath.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    public GameObject DeathZone;
+    public GameObject Player;
+    public GameObject Respawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider col)
+    {
+        Debug.Log("Salut connard!");
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Meurt fdp");
+            Player.GetComponent<MeshRenderer>().enabled = false;
+            Player.transform.localPosition = Respawn.transform.localPosition;
+        }
+        else if (Player.transform.localPosition == Respawn.transform.position)
+        {
+            Debug.Log("Salut enculé!");
+            Player.GetComponent<MeshRenderer>().enabled = true;
+        }
+
+    }
+}
